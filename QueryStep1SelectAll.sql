87443aa30251735166374fbf5a28bc3100562136
AnthonySanchez2000 <123671284+AnthonySanchez2000@users.noreply.github.com>
2023-01-26 15:11:41 -0500

    Add files via upload

---
+++ QueryStep1SelectAll.sql
@@ -0,0 +1,3 @@
+select *
+From PortfolioProject..WorldCauseofDeaths1990
+order by 1,2
---
+++ QueryStep3SelectUSCanadaMex.sql
@@ -0,0 +1,14 @@
+select *
+From PortfolioProject..WorldCauseofDeaths1990
+where Country like '%United States%'
+order by 1,2
+
+select *
+From PortfolioProject..WorldCauseofDeaths1990
+where Country like '%Canada%'
+order by 1,2
+
+select *
+From PortfolioProject..WorldCauseofDeaths1990
+where Country like '%Mexico%'
+order by 1,2
