e3b5e5719294fe4c09cbc07305dd7287ecd06d68
AnthonySanchez2000 <123671284+AnthonySanchez2000@users.noreply.github.com>
2023-03-13 13:36:18 -0400

    Delete QueryStep2SelectAllDeathTotal.sql

--- QueryStep2SelectAllDeathTotal.sql
+++
@@ -1,9 +0,0 @@
-select * , [Executions] + [Meningitis] + [Dementias] + [Parkinson's disease] + [Nutritional deficiencies] + [Malaria] + [Drowning] 
-+ [Interpersonal violence] + [Maternal disorders] + [HIV/AIDS] + [Drug use disorders] + [Tuberculosis] + [Cardiovascular diseases] 
-+ [Lower respiratory infections] + [Neonatal disorders] + [Alcohol use disorders] + [Self-harm] + [Exposure to forces of nature] 
-+ [Diarrheal disease] + [Environmental heat and cold exposure] + [Neoplasms] + [Conflict and terrorism] + [Diabetes mellitus] 
-+ [Chronic kidney disease] + [Poisonings] + [Protein-energy malnutrition] + [Terrorism] + [Road injuries] + [Chronic respiratory diseases] 
-+ [Chronic liver diseases] + [Digestive diseases] + [Fire, heat, and hot substances] + [Acute hepatitis]
-as Total
-From PortfolioProject..WorldCauseofDeaths1990
-order by 1, 2
